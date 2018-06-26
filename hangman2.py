b309a397bb4c707eb20f7fd8bc40ae6887e9e7c5
norihironorihiro <nontandou@yahoo.co.jp>
2018-06-26 13:41:56 +0900

    my second commit

---
+++ hangman2.py
@@ -0,0 +1,40 @@
+ngman
+
+def hangman(word):
+    wrong = 0
+    strages = ["",
+               "______      ",
+               "|           ",
+               "|     |     ",
+               "|     0     ",
+               "|    /|_    ",
+               "|    / _    ",
+               "|           "
+               ]
+    rletters = list(word)
+    board = ["_"] * len(word)
+    win = False
+    print("ハングマンへようこそ!")
+    while wrong < len(strages) - 1:
+        print("\n")
+        msg = "1文字を予想してね"
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = "$"
+        else:
+            wrong += 1
+        print(" ".join(board))
+        e = wrong + 1
+        print("\n".join(strages[0:e]))
+        if "_" not in board:
+            print("あなたの勝ち")
+            print(" ".join(board))
+            win = True
+            break
+    if not win:
+        print("\n".join(strages[0:wrong+1]))
+        print("あなたの負け！正解は{}.".format(word))
+
+hangman("momon")
